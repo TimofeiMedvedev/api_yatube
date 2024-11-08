d714b9f9bdca8cb5be1909f57b5a09dea815f814
Timofei Medvedev <medtv78@gmail.com>
2024-11-08 11:16:31 +0300

    add all review

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,10 +1,9 @@
-from api.permissions import IsAuthorOrReadOnly
 from django.contrib.auth import get_user_model
-from django.core.exceptions import PermissionDenied
-from django.shortcuts import get_object_or_404
-from posts.models import Comment, Group, Post
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
+
+from api.permissions import IsAuthorOrReadOnly
+from posts.models import Comment, Group, Post
 
 from .serializers import CommentSerializer, GroupSerializer, PostSerializer
 
